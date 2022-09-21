87d9d555dfb29f1e550bec0e426101633b4ec89d
VoVanHoang <vvhoang18092001@gmail.com>
2022-09-21 23:01:17 +0700

    Update Unit6_Advanced_Exercise

--- Unit6_BitMask_Exercise/main.c
+++ Unit6_BitMask_Exercise/main.c
@@ -61,7 +61,7 @@ void delay(int number_of_seconds)
     // Converting time into milli_seconds
     int milli_seconds = 1000 * number_of_seconds;
 
-    // Storing start time
+    // Storing start the time
     clock_t start_time = clock();
 
     // looping till required time is not achieved
